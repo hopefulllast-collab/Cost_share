ac00c39e3c7614b65885bac0f50df7dec5bbd7fb
hopefulllast-collab <hopefulllast@gmail.com>
2026-04-14 21:59:56 +0300

    Reduce session timeout duration to 60 seconds

--- includes/auth_check.php
+++ includes/auth_check.php
@@ -2,7 +2,7 @@
 require_once __DIR__ . '/session_manager.php';
 
 // Session timeout duration in seconds (4 minutes = 240 seconds)
-define('SESSION_TIMEOUT', 240);
+define('SESSION_TIMEOUT', 60);
 
 function checkAuth($allowed_roles = [])
 {
