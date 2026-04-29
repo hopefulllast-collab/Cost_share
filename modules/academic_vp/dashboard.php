c01e1e064ae2f63f8481b0f7e9f80ebf6dd7d0a7
Deployment Agent <deploy@costshare.local>
2026-04-29 14:03:31 +0300

    Resolve merge conflict and update academic vp dashboard

--- modules/academic_vp/dashboard.php
+++ modules/academic_vp/dashboard.php
@@ -100,7 +100,8 @@ require_once '../../includes/academic_translations.php';
         </div>
         <?php include '../../includes/footer.php'; ?>
     </div>
-    <script src="../../assets/js/bilingual.js"></script>      
+    <script src="../../assets/js/bilingual.js"></script>
+    <script src="../../assets/js/responsive.js"></script>
 </body>
 
 </html>
