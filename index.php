50a8c2147888e287419b948b2a2b1ff3c76d52cd
hopefulllast-collab <hopefulllast@gmail.com>
2026-05-27 01:51:29 +0300

    Add favicon link to index.php
    Added a favicon link to the HTML head section.

--- index.php
+++ index.php
@@ -113,6 +113,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['login'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title data-en="Debre Markos University - Cost Sharing" data-am="ደብረ ማርቆስ ዩኒቨርሲቲ - ወጪ መጋራት">Debre Markos University
         - Cost Sharing</title>
+    <link rel="icon" type="image/png" href="assets/images/dmulogo.png">
     <link rel="stylesheet" href="assets/css/index.css?v=11">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link
